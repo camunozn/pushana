0423bd276b7a47ce89b1a0aa79ade54100da94ed
camunozn <camunozn89@gmail.com>
2023-03-02 19:27:46 -0500

    Add responsive design to home route

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,36 +7,38 @@ const Home = () => {
   return (
     <div className={styles.home}>
       <div className={styles['section-hero']}>
-        <div className={styles['hero-heading']}>
-          <h1 className={styles['hero-heading__text']}>
-            Gestión de proyectos de construcción hecha
-            <TypeAnimation
-              sequence={[
-                'fácil',
-                1000,
-                'efectiva',
-                1000,
-                'eficiente',
-                1000,
-                'accesible',
-                1000,
-              ]}
-              speed={50} // Custom Speed from 1-99 - Default Speed: 40
-              wrapper="span" // Animation will be rendered as a <span>
-              repeat={Infinity} // Repeat this Animation Sequence infinitely
-            />
-          </h1>
-          <div className={styles['hero-heading__buttons']}>
-            <a className="btn btn__primary" href="">
-              Únete a la comunidad
-            </a>
-            <a className="btn btn__secondary" href="">
-              Contáctanos
-            </a>
+        <div className={styles['hero-main']}>
+          <div className={styles['hero-main__heading']}>
+            <h1 className={styles['hero-main__heading__text']}>
+              Gestión de proyectos de construcción hecha
+              <TypeAnimation
+                sequence={[
+                  'fácil',
+                  1000,
+                  'efectiva',
+                  1000,
+                  'eficiente',
+                  1000,
+                  'accesible',
+                  1000,
+                ]}
+                speed={50} // Custom Speed from 1-99 - Default Speed: 40
+                wrapper="span" // Animation will be rendered as a <span>
+                repeat={Infinity} // Repeat this Animation Sequence infinitely
+              />
+            </h1>
+          </div>
+          <div className={styles['hero-main__img']}>
+            <img src={heroImg} alt="Hero image" />
           </div>
         </div>
-        <div className={styles['hero-img']}>
-          <img src={heroImg} alt="Hero image" />
+        <div className={styles['hero-buttons']}>
+          <a className="btn btn__primary" href="">
+            Descubre más
+          </a>
+          <a className="btn btn__secondary" href="">
+            Contáctanos
+          </a>
         </div>
       </div>
     </div>
