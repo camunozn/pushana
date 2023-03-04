5a146749c31756c63031ae093ab7836675b517be
camunozn <camunozn89@gmail.com>
2023-03-03 22:26:43 -0500

    Add images to hero img array

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,6 +1,8 @@
 import img1 from './heroImages/hero-img-easy.png';
 import img2 from './heroImages/hero-img-effective.png';
 import img3 from './heroImages/hero-img-efficient.png';
-import img4 from './heroImages/hero-img-transparent.png';
+import img4 from './heroImages/hero-img-accesible.png';
+import img5 from './heroImages/hero-img-transparent.png';
+import img6 from './heroImages/hero-img-clear.png';
 
-export default [img1, img2, img3, img4];
+export default [img1, img2, img3, img4, img5, img6];
