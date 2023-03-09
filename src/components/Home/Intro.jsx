82f5d3ce6e408bdd041e1ab0372a8ee529b9acf6
camunozn <camunozn89@gmail.com>
2023-03-09 10:09:11 -0500

    Update section intro from home page

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -4,18 +4,17 @@ import styles from './Intro.module.css';
 const Intro = () => {
   return (
     <section className={styles['intro']}>
-      <h2 className={styles['intro__heading']}>
-        Pushana es una empresa que busca cambiar la forma de planificar y
-        controlar los proyectos de construcción en el país.
-      </h2>
-      <p className={styles['intro__text']}>
-        Nuestra aplicación está diseñada para facilitar la planificación y el
-        control de proyectos de construcción, haciendo a estos procesos
-        eficientes, claros y accesibles para todos. Especialmente pensada para
-        potenciar a pequeños y medianos constructores.
-      </p>
-      <div className={styles['intro__line']}>
-        <span></span>
+      <div className={styles['intro__container']}>
+        <h2 className={styles['intro__heading']}>
+          Cambiamos la forma de planificar y controlar tus proyectos de
+          construcción.
+        </h2>
+        <p className={styles['intro__text']}>
+          Nuestra aplicación está diseñada para facilitar la planificación y el
+          control de proyectos de construcción, haciendo a estos procesos
+          eficientes, claros y accesibles para todos. Especialmente pensada para
+          potenciar a pequeños y medianos constructores.
+        </p>
       </div>
     </section>
   );
