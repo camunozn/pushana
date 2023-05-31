55e074e45bde15cf70fe853b1e06a829e3b2debc
César Muñoz <camunozn89@gmail.com>
2023-05-31 12:33:23 -0500

    Fix form layout

--- src/components/organisms/registration-form/RegistrationForm.jsx
+++ src/components/organisms/registration-form/RegistrationForm.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import styles from './RegistrationForm.module.css';
 import HeadingSmall from '../../molecules/heading-small/HeadingSmall';
+import MarginBox from '../../atoms/margin-box/MarginBox';
+import styles from './RegistrationForm.module.css';
 
 const RegistrationForm = props => {
   return (
     <div className="registration-form">
-      <HeadingSmall
-        title={props.formData.heading.title}
-        text={props.formData.heading.text}
-      />
+      <MarginBox>
+        <HeadingSmall
+          title={props.formData.heading.title}
+          text={props.formData.heading.text}
+        />
+      </MarginBox>
     </div>
   );
 };
--- src/modules/section-intro/Introduction.jsx
+++ src/modules/section-intro/Introduction.jsx
@@ -3,7 +3,6 @@ import HeadingLarge from '../../components/molecules/heading-large/HeadingLarge'
 import CardsBox from '../../components/molecules/cards-box/CardsBox';
 import RegistrationForm from '../../components/organisms/registration-form/RegistrationForm';
 import styles from './Introduction.module.css';
-import MarginBox from '../../components/atoms/margin-box/MarginBox';
 
 const Introduction = props => {
   return (
@@ -11,11 +10,7 @@ const Introduction = props => {
       <div className={styles['intro__container']}>
         <HeadingLarge title={props.heading.title} text={props.heading.text} />
         {props.cardsList && <CardsBox cardsList={props.cardsList} />}
-        {props.formData && (
-          <MarginBox>
-            <RegistrationForm formData={props.formData} />
-          </MarginBox>
-        )}
+        {props.formData && <RegistrationForm formData={props.formData} />}
       </div>
     </section>
   );
