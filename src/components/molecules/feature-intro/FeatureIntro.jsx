417a04ced93a87341da9e3883299b44d179fa7c4
César Muñoz <camunozn89@gmail.com>
2024-07-06 20:44:12 -0600

    Update main page design

--- src/components/molecules/feature-intro/FeatureIntro.jsx
+++ src/components/molecules/feature-intro/FeatureIntro.jsx
@@ -3,19 +3,19 @@ import styles from './FeatureIntro.module.css';
 
 const FeatureIntro = props => {
   return (
-    <div className={styles['feature-heading']}>
-      <div className={styles[`feature-heading__container--${props.accent}`]}>
-        <h3 className={styles['feature-heading__title']}>
-          {props.title.start}{' '}
-          <span className={styles[`accent--${props.accent}`]}>
-            {props.title.middle}{' '}
-          </span>
-          {props.title.end}
-        </h3>
-        <p className={styles['feature-heading__text']}>{props.text}</p>
-        {/* <div className={styles[`feature-heading__img-box--${props.accent}`]}>
+    <div className={styles['feature']}>
+      <div className={styles['feature__container']}>
+        <div className={styles['feature__heading']}>
           <img src={props.image} alt="Feature image" />
-        </div> */}
+          <h3 className={styles['feature__title']}>
+            {props.title.start}{' '}
+            <span className={styles[`accent--${props.accent}`]}>
+              {props.title.middle}{' '}
+            </span>
+            {props.title.end}
+          </h3>
+        </div>
+        <p className={styles['feature__text']}>{props.text}</p>
       </div>
     </div>
   );
